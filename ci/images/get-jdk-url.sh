ed8f866a529734851f0598b85aee27583b05a136
mrpotatoserver <estoespersonal1968@gmail.com>
2020-10-01 18:34:40 +0200

    second commit

---
+++ ci/images/get-jdk-url.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+	java8)
+		 echo "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz"
+	;;
+	java11)
+		 echo "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.8%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.8_10.tar.gz"
+	;;
+	java15)
+		 echo "https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15%2B36/OpenJDK15U-jdk_x64_linux_hotspot_15_36.tar.gz"
+	;;
+  *)
+		echo $"Unknown java version"
+		exit 1
+esac
